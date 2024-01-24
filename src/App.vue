a6742bed799ba6fbec8dc6d58a2ee77747e2b0fd
Adrian Bueno <32718131+adrian7123@users.noreply.github.com>
2024-01-24 18:45:43 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="text-6xl">Adrian Bueno</h1>
+    <h2>Desenvolvedor Full-Stack | Nodejs | Flutter</h2>
+  </div>
+</template>
+
+<style scoped></style>
