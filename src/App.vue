18775d4667ddb4db56053e97e31a375fda23c096
Adrian Bueno <32718131+adrian7123@users.noreply.github.com>
2024-01-24 19:17:27 -0300

    add assets

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Node from "@/assets/icons/node.png";
+import Flutter from "@/assets/icons/flutter.png";
+import Rust from "@/assets/icons/rust.png";
+</script>
 
 <template>
   <div>
-    <h1 class="text-6xl">Adrian Bueno</h1>
-    <h2>Desenvolvedor Full-Stack | Nodejs | Flutter</h2>
+    <h1 class="text-6xl mb-2">Adrian Bueno</h1>
+    <div class="flex items-center">
+      Desenvolvedor Full-Stack |
+      <img class="h-10 mx-2" :src="Node" />
+      Nodejs |
+      <img class="h-10 mx-1" :src="Flutter" />
+      Flutter |
+      <img class="h-10 mx-2" :src="Rust" />
+      Rust
+    </div>
   </div>
 </template>
 
